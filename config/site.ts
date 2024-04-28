3a041163c611536dab51c1573e0caa9c98298de4
ayushkumar3820 <ayushkumarnbd125@gamil.com>
2024-04-28 09:47:08 +0530

    bug fix

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Ayush Kumar",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Ayush Kumar",
+  links: {
+    twitter: "https://twitter.com/jollyshopland",
+    github: "https://github.com/ayushkumar3820/",
+    personalSite: "https://jamesshopland.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
